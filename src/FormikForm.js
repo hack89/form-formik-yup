97e0cca8f380d7e1814786fd356f647af6c6e612
hack89 <hackinsta89@gmail.com>
2020-03-24 18:12:18 +0100

    adding auto suggestion

--- src/FormikForm.js
+++ src/FormikForm.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Formik} from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios'
 import Error from './Error'
+import Autosuggest from 'react-autosuggest'
 
 const validationSchema = Yup.object().shape({
     name: Yup.string()
@@ -11,13 +13,18 @@ const validationSchema = Yup.object().shape({
     
     email: Yup.string()
         .email('Must be a valid email')
-        .required('Must enter an email')
+        .required('Must enter an email'),
+    country: Yup.string()
+        .required('Must choose country')
 })
 
 const FormikForm = () => {
+    const [country, setCountry] = useState('')
+    const [suggestions, setSuggestions] = useState([])
+
     return (
         <Formik
-            initialValues={{name: '', email: ''}}
+            initialValues={{name: '', email: '', country: ''}}
             validationSchema={validationSchema}
             onSubmit={(values, {setSubmitting, resetForm})=>{
                 setSubmitting(true)
@@ -25,11 +32,12 @@ const FormikForm = () => {
                     alert(JSON.stringify(values, null, 2));
                     resetForm()
                     setSubmitting(false)
+                    setCountry('')
                 },500)
             }}
             >
 
-            {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting})=> ( 
+            {({values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue})=> ( 
                 
                 <form onSubmit={handleSubmit}>
                 
@@ -61,7 +69,60 @@ const FormikForm = () => {
                             />
                             <Error touched={touched.email} message={errors.email}/>
                     </div>
-                    <div className="input-ro">
+                    <div className="input-row">
+                        <label htmlFor="country">Country</label>
+                        <Autosuggest 
+                            inputProps={{
+                                placeholder: 'Type your country',
+                                autoComplete: 'abcd',
+                                name: 'country',
+                                id: 'country',
+                                value: country,
+                                onChange: (e, {newValue})=> {
+                                    setCountry(newValue)
+                                },
+                                className: touched.name && errors.name ? 'has-error' : null
+                        }}
+                        suggestions={suggestions}
+                        onSuggestionsFetchRequested={async ({value}) => {
+                            if(!value){
+                                setSuggestions([]);
+                                return;
+                            }
+                            try {
+                                const res = await axios.get(`https://restcountries.eu/rest/v2/name/${value}`);
+                                setSuggestions(res.data.map(row => {
+                                    return {
+                                            name: row.name,
+                                            flag: row.flag
+                                           }
+                                }))
+                            } catch(e){
+                                setSuggestions([])
+                            }
+                        }}
+                        onSuggestionsClearRequested={()=> {
+                            setSuggestions([])
+                        }}
+                        onSuggestionSelected={(e, {suggestion, method})=> {
+                            if(method === 'enter'){
+                                e.preventDefault()
+                            }
+                            setCountry(suggestion.name)
+                            setFieldValue('country', suggestion.name)
+                        }}
+                        getSuggestionValue={(suggestion)=> suggestion.name}
+                        renderSuggestion={suggestion => (
+                                <div>
+                                    <img src={suggestion.flag} style={{width: '25px', marginRight: '6px'}} alt={suggestion.name}/>
+                                    {suggestion.name}
+                                </div>
+                                )}
+                        />
+                        <Error touched={touched.country} message={errors.country}/>
+                    </div>
+
+                    <div className="input-row">
                         <button type="submit" disabled={isSubmitting}>Submit</button>
                     </div>
                 </form>
